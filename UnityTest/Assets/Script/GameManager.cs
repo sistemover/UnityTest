710b5280003c1082ecb08d609fd262bbabe3830a
sistemover <31515147+sistemover@users.noreply.github.com>
2018-02-09 12:46:22 -0300

    Pre-Añadir Gamepad.
    Plataforma : Standalone.

---
+++ UnityTest/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.CrossPlatformInput;
+//using Sistemover.TouchGamePad;
+
+public class GameManager : MonoBehaviour 
+{
+	public bool A = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//A = CrossPlatformInputManager.GetButtonDown ("A");
+		if (A) {
+			Debug.Log ("pulsó A");
+		}
+	}
+}
